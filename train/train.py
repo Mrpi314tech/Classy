7871a4c6563b53e92f5ce53486ae1121bb691a30
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2024-09-18 09:53:39 -0400

    Update train.py

--- train/train.py
+++ train/train.py
@@ -54,11 +54,11 @@ X_train = np.array(X_train)
 y_train = np.array(y_train)
 
 # Hyper-parameters 
-num_epochs = 1000
+num_epochs = 500
 batch_size = 64
 learning_rate = 0.001
 input_size = len(X_train[0])
-hidden_size = 4
+hidden_size = 8
 hidden_size_2=0
 output_size = len(tags)
 print(str(input_size)+' > '+str(hidden_size)+' > '+str(output_size))
