fe6e57e1c74ac92ef90ff52722c98f007c0d5b38
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2024-10-28 09:21:06 -0400

    Update nltk_utils.py

--- train/nltk_utils.py
+++ train/nltk_utils.py
@@ -5,33 +5,17 @@ from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
 def tokenize(sentence):
-    """
-    split sentence into array of words/tokens
-    a token can be a word or punctuation character, or number
-    """
+    # split the sentence into individual words
     return nltk.word_tokenize(sentence)
 
 
 def stem(word):
-    """
-    stemming = find the root form of the word
-    examples:
-    words = ["organize", "organizes", "organizing"]
-    words = [stem(w) for w in words]
-    -> ["organ", "organ", "organ"]
-    """
+    # stem the word to find the root and connect similar words
     return stemmer.stem(word.lower())
 
 
 def bag_of_words(tokenized_sentence, words):
-    """
-    return bag of words array:
-    1 for each known word that exists in the sentence, 0 otherwise
-    example:
-    sentence = ["hello", "how", "are", "you"]
-    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
-    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
-    """
+    # return bow array so that the nn can understand the data.
     # stem each word
     sentence_words = [stem(word) for word in tokenized_sentence]
     print(sentence_words)
